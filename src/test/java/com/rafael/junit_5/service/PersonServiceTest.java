b555dab11aa189ce7a5b1ac169b18c3393072aeb
RafaelTS <rafaeltorress@gmail.com>
2025-07-10 14:50:07 -0300

    simple refactory

--- src/test/java/com/rafael/junit_5/service/PersonServiceTest.java
+++ src/test/java/com/rafael/junit_5/service/PersonServiceTest.java
@@ -1,6 +1,7 @@
 package com.rafael.junit_5.service;
 
 import com.rafael.junit_5.model.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonServiceTest {
 
+    Person person;
+
+    @BeforeEach
+    void setup() {
+        person = new Person("Rafael", "Teixeira", "Street Teixeira, 100", "rafael@gmail.com");
+    }
+
     @DisplayName("When Create a Person with Success Should return a Person Object")
     @Test
     void testCreatPersonWhenSucessReturnPersonObject() {
         // arrange
         IPersonService service = new PersonService();
-
-        Person person = new Person("Rafael", "Teixeira", "Street Teixeira, 100", "rafael@gmail.com");
         // act
         Person actual = service.createPerson(person);
         // assert
@@ -27,13 +33,11 @@ public class PersonServiceTest {
     void testCreatPersonWhenSucessShoulContainsFirstNameInReturnedPersonObject() {
         // arrange
         IPersonService service = new PersonService();
-
-        Person person = new Person("Rafael", "Teixeira", "Street Teixeira, 100", "rafael@gmail.com");
         // act
         Person actual = service.createPerson(person);
         // assert
 
         assertNotNull(actual, () -> "createPerson should not have returned null!");
-        assertEquals(person.getFirtsName(), actual.getFirtsName(), () -> "createPerson should not have returned null!");
+        assertEquals(person.getFirtsName(), actual.getFirtsName(), () -> "createPerson should not returned First Name null or different!");
     }
 }
